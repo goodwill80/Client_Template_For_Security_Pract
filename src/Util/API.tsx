6e3e75203586972b1e83bfa42c7444fbe63176ac
jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2024-03-25 22:35:56 +0800

    piw

--- src/Util/API.tsx
+++ src/Util/API.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { getCookie } from "typescript-cookie";
+import axios from 'axios';
+import { getCookie } from 'typescript-cookie';
 
 export type ContactModel = {
   contactName: string;
@@ -9,10 +9,10 @@ export type ContactModel = {
 };
 
 const getAuthorizationFromLocalStorage = (): string | null => {
-  const getFromLocalStorage = sessionStorage.getItem("userdetails");
+  const getFromLocalStorage = sessionStorage.getItem('userdetails');
   if (getFromLocalStorage) {
     const user = JSON.parse(getFromLocalStorage as string);
-    return "Basic " + window.btoa(user.email + ":" + user.password);
+    return 'Basic ' + btoa(user.email + ':' + user.password);
   } else {
     return null;
   }
@@ -24,42 +24,68 @@ const getAuthorizationFromLocalStorage = (): string | null => {
 
 // API Instance
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: 'http://localhost:8080',
   // timeout: 70000,
-  headers: {
-    "content-type": "application/json",
-    // Authorization: getAuthorizationFromLocalStorage(),
-    // // authorizationToken: getTokenFromLocalStorage(),
-    // "X-XSRF-TOKEN": sessionStorage.getItem("XSRF-TOKEN") || "",
-  },
+  // headers: {
+  // 'content-type': 'application/json',
+  // Authorization: getAuthorizationFromLocalStorage(),
+  // authorizationToken: getTokenFromLocalStorage(),
+  // 'X-XSRF-TOKEN': sessionStorage.getItem('XSRF-TOKEN') || '',
+  // },
 });
+
+// 'Authorization',
+//   'Basic ' + window.btoa(this.user.email + ':' + this.user.password);
+// 'X-Requested-With', 'XMLHttpRequest';
 
 // To inject into header for all outgoing request
-API.interceptors.request.use((config) => {
-  if (sessionStorage.getItem("XSRF-TOKEN")) {
-    config.headers["X-XSRF-TOKEN"] = sessionStorage.getItem("XSRF-TOKEN");
-    config.headers["content-type"] = "application/json";
-  }
-  if (sessionStorage.getItem("userdetails")) {
-    config.headers["Authorization"] = getAuthorizationFromLocalStorage();
-    config.headers["content-type"] = "application/json";
-  }
+// API.interceptors.request.use((config) => {
+//   if (sessionStorage.getItem('XSRF-TOKEN')) {
+//     config.headers['X-XSRF-TOKEN'] = sessionStorage.getItem('XSRF-TOKEN');
+//     config.headers['content-type'] = 'application/json';
+//     // config.headers['X-Requested-With'] = 'XMLHttpRequest';
+//   }
+//   if (sessionStorage.getItem('userdetails')) {
+//     config.headers['Authorization'] = getAuthorizationFromLocalStorage();
+//     config.headers['content-type'] = 'application/json';
+//     // config.headers['X-Requested-With'] = 'XMLHttpRequest';
+//   }
 
-  return config;
-});
+//   return config;
+// });
 
-export const LoginAPI = async (user: { email: string; pwd: string }) => {
+export const LoginAPI = async (user: {
+  email: string;
+  pwd: string;
+  authStatus: string | '';
+}) => {
   // window.sessionStorage.setItem("userdetails", JSON.stringify(user));
   // let xsrf = getCookie("XSRF-TOKEN")!;
   // window.sessionStorage.setItem("XSRF-TOKEN", xsrf);
-  window.sessionStorage.setItem("userdetails", JSON.stringify(user));
+  user.authStatus = 'AUTH';
+  window.sessionStorage.setItem('userdetails', JSON.stringify(user));
   try {
-    const response = await API.get("/user", { withCredentials: true });
-    const xsrf = getCookie("XSRF-TOKEN")!;
+    const getFromLocalStorage = sessionStorage.getItem('userdetails');
+    const user = JSON.parse(getFromLocalStorage as string);
+    const response = await API.get('/user', {
+      withCredentials: true,
+      headers: {
+        Authorization: 'Basic ' + btoa(user.email + ':' + user.pwd),
+        'content-type': 'application/json',
+        // 'X-Requested-With': 'XMLHttpRequest',
+      },
+
+      // headers: {
+      //   Authorization: getAuthorizationFromLocalStorage(),
+      //   'content-type': 'application/json, text/plain, */*',
+      // },
+    });
     console.log(response);
-    console.log(xsrf);
-    window.sessionStorage.setItem("XSRF-TOKEN", xsrf);
-    return response;
+    const xsrf = getCookie('XSRF-TOKEN')!;
+    // window.sessionStorage.setItem('userdetails', JSON.stringify());
+    window.sessionStorage.setItem('XSRF-TOKEN', xsrf);
+    console.log(response);
+    return response.data;
   } catch (e) {
     console.log(e);
   }
@@ -67,7 +93,7 @@ export const LoginAPI = async (user: { email: string; pwd: string }) => {
 
 export const RegisterAPI = async (user: { email: string; pwd: string }) => {
   try {
-    const response = await API.post("/register", user);
+    const response = await API.post('/register', user);
     return response.data;
   } catch (e) {
     console.log(e);
@@ -76,7 +102,7 @@ export const RegisterAPI = async (user: { email: string; pwd: string }) => {
 
 export const getNoticesAPI = async () => {
   try {
-    const response = await API.get("/notice");
+    const response = await API.get('/notice');
     return response.data;
   } catch (e) {
     console.log(e);
@@ -85,7 +111,7 @@ export const getNoticesAPI = async () => {
 
 export const sendContactAPI = async (contactDetails: ContactModel) => {
   try {
-    const response = await API.post("/contact", contactDetails);
+    const response = await API.post('/contact', contactDetails);
     return response.data;
   } catch (e) {
     console.log(e);
